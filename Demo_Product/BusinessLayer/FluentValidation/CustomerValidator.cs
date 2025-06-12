399ccec242bc3086b1aa258cbaa15214a661e081
Asli Şahin <aslisahin0908@gmail.com>
2025-06-12 17:41:25 +0300

    Demo-Product-App
    Demo-Product-App

---
+++ Demo_Product/BusinessLayer/FluentValidation/CustomerValidator.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.FluentValidation
+{
+    public class CustomerValidator : AbstractValidator<Customer>
+    {
+        public CustomerValidator()
+        {
+            RuleFor(x => x.City).NotEmpty().WithMessage("Şehir adı boş bırakılamaz");
+            RuleFor(x => x.Name).NotEmpty().WithMessage("İsim alanı boş bırakılamaz");
+
+
+        }
+    }
+}
---
+++ Demo_Product/BusinessLayer/FluentValidation/ProductValidator.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.FluentValidation
+{
+    public class ProductValidator : AbstractValidator<Product>
+    {
+        public ProductValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Ürün adı boş bırakılamaz");
+            RuleFor(x => x.Name).MinimumLength(3).WithMessage("Ürün adı en az 3 karakter olmalıdır");
+            RuleFor(x=>x.Stock).NotEmpty().WithMessage("Stok bilgisi boş bırakılamaz");
+            RuleFor(x=>x.Price).NotEmpty().WithMessage("Fiyat bilgisi boş bırakılamaz");
+        }
+    }
+}
